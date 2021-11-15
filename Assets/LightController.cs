aa3fd7e38142973245e9b0974526266cc2708f8c
tomur1 <32645778+tomur1@users.noreply.github.com>
2021-11-15 17:32:34 +0100

    Initial Commit

---
+++ Assets/LightController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightController : MonoBehaviour
+{
+    public Light2D light;
+
+    public bool exit;
+
+    public bool pointLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            var bounds = GetComponent<BoxCollider2D>().bounds;
+
+            var colliderPosition = transform.position;
+            Debug.Log(colliderPosition);
+            var playerPos = other.gameObject.transform.position;
+
+            var distanceFromCenter = playerPos - colliderPosition;
+
+            var positionPercentage = distanceFromCenter.x / bounds.extents.x;
+
+            if (pointLight)
+            {
+                positionPercentage = 1 - Mathf.Abs(positionPercentage);
+            }
+            else
+            {
+                if (exit)
+                {
+                    positionPercentage = (1 + positionPercentage) / 2;
+                    positionPercentage = Mathf.Clamp(positionPercentage, 0, 1);
+                }
+                else
+                {
+                    positionPercentage = (1 + positionPercentage) / 2;
+                    positionPercentage = Mathf.Clamp(positionPercentage, 0, 1);
+                    positionPercentage = 1 - positionPercentage;
+                }
+            }
+            
+            light.intensity = positionPercentage;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float jumpForce;
+    public float movementSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalInput = Input.GetAxisRaw("Horizontal");
+        var verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Math.Abs(verticalInput - 1) < 0.01f || Input.GetButtonDown("Jump"))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+        }
+
+        rb2d.velocity = new Vector2(horizontalInput * movementSpeed * Time.deltaTime, rb2d.velocity.y);
+    }
+}
